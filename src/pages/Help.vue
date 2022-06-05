9de2378e86ca5a47c9cb6f2c4eb9b041c1ea90cc
boralex <boris.alexandre.rose@gmail.com>
2022-06-05 03:03:26 +0200

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="q-pa-lg">
+      <h5 class="q-mt-none"> Help</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci deserunt unde, voluptate quaerat a suscipit. Ducimus temporibus quasi quisquam quos inventore quidem ipsum aperiam, corporis sit quas accusamus sed doloremque?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem facere odio exercitationem amet officiis, eos, est numquam earum deleniti omnis nemo in, reiciendis quis ab sunt soluta iusto vero repudiandae?</p>
+  </q-page>
+</template>
+
+<script>
+  
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'HelpComponent'
+  });
+</script>
